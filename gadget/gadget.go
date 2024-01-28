76f445456570a9d45dbc1071a08463c102f201ed
Nikos Bompetsis <n.bompetsis@opap.gr>
2024-01-28 16:01:38 +0200

    Added gadget packege (learning purposes)

---
+++ gadget/gadget.go
@@ -0,0 +1,38 @@
+package gadget
+
+import "fmt"
+
+// TapePlayer type
+type TapePlayer struct {
+	Batteries string
+}
+
+// Play method of type
+func (t TapePlayer) Play(song string) {
+	fmt.Println("Playing song", song)
+}
+
+// Stop method of type
+func (t TapePlayer) Stop() {
+	fmt.Println("Stopped!")
+}
+
+// TapeRecorder type
+type TapeRecorder struct {
+	Microphone int
+}
+
+// Play method of type
+func (t TapeRecorder) Play(song string) {
+	fmt.Println("Playing song", song)
+}
+
+// Record method of type
+func (t TapeRecorder) Record() {
+	fmt.Println("Recording")
+}
+
+// Stop method of type
+func (t TapeRecorder) Stop() {
+	fmt.Println("Stopped!")
+}
